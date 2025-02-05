5c3e48a17e91f7d3e9dd09fa9222efd4e07768b1
CRI88 <christianrieigl@monlau.com>
2025-02-05 15:55:40 +0100

    Primera subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
+import { FeaturedComponent } from './components/featured/featured.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ContentComponent, FeaturedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
