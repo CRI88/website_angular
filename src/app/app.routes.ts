344b9bcee7088fb185f116dc190c2454342e24a6
CRI88 <christianrieigl@monlau.com>
2025-02-13 19:18:52 +0100

    hasta content 1 menos las fotos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ContentComponent } from './components/content/content.component';
+import { Content1Component } from './components/content1/content1.component';
+import { Content2Component } from './components/content2/content2.component';
+import { Content3Component } from './components/content3/content3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ /*   { path: '', redirectTo: 'content', pathMatch: 'full' }, // Página por defecto
+    { path: 'content', component: ContentComponent },
+    { path: 'content1', component: Content1Component },
+    { path: 'content2', component: Content2Component },
+    { path: 'content3', component: Content3Component },
+
+    // 🔁 Redirecciones personalizadas
+    { path: 'o', redirectTo: 'content1', pathMatch: 'full' },
+    { path: 'p', redirectTo: 'content2', pathMatch: 'full' },
+    { path: 'q', redirectTo: 'content3', pathMatch: 'full' }, */
+];
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  var1 = '© 2025, Epic Games, Inc. Todos los derechos reservados. Epic, Epic Games, el logotipo de Epic Games, Fortnite, el logotipo de Fortnite, Unreal, Unreal Engine, el logotipo de Unreal Engine, Unreal Tournament y el logotipo de Unreal Tournament son marcas comerciales o marcas registradas de Epic Games, Inc. tanto en Estados Unidos de América como en el resto del mundo. Otras marcas o nombres de productos son marcas comerciales de sus respectivos propietarios. Nuestros sitios web pueden incluir enlaces a otros sitios y recursos ofrecidos por terceros. Estos enlaces solo se ofrecen para tu mayor comodidad. Epic Games no tiene control sobre el contenido de esos sitios y recursos, y no acepta ninguna responsabilidad por ellos ni por cualquier pérdida o daño que pueda ocasionar el uso que haces de ellos.';
+  var2 = {terminos: 'Términos de servicio', politica1: 'Política de privacidad', politica2: 'Política de reembolso de la tienda', 
+    listado: 'Listado de editoras'
+  }
 }
